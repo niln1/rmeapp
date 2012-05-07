f2741833954df14795b2b004b50d8fa4ce428ec9
niz1992 <niz@rose-hulman.edu>
2012-05-07 19:59:52 -0400

    asdasda

--- public/static/airport.js
+++ public/static/airport.js
@@ -3,7 +3,7 @@
          airport: function(array) {
 			
 			var self = $(this);
-			var chars = ['a','b','c','d','e','f','g',' ','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-'];
+			var chars = ['.','a','b','c','d','e','f','g',' ','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-'];
 			var longest = 0;
 			var items = items2 = array.length;
 
